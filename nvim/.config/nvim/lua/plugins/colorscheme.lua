b2793001df89f96ca547dc4fee3933bfb12ded3e
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-05-10 12:51:16 +0530

    change rose-pine background and add git current line blame in git signs

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,15 @@ return {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
+            require("rose-pine").setup({
+                variant = "main",
+                dark_variant = "main",
+                palette = {
+                    main = {
+                        base = "#000000"
+                    }
+                }
+            })
             vim.cmd("colorscheme rose-pine")
         end,
     },
