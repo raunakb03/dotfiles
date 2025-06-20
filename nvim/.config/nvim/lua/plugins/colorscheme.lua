f98b2ea9630a15c929e3e5485db16657e331ae2b
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-06-20 22:42:50 +0530

    new colorschemes baybay

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -12,7 +12,37 @@ return {
                     }
                 }
             })
-            vim.cmd("colorscheme rose-pine")
+            -- vim.cmd("colorscheme rose-pine")
         end,
     },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                on_colors = function(colors)
+                    colors.bg = "#000000"
+                end
+            })
+            vim.cmd("colorscheme tokyonight-night")
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                color_overrides = {
+                    mocha = {
+                        base = "#000000",
+                    },
+                },
+            })
+            -- vim.cmd("colorscheme catppuccin")
+        end,
+    }
 }
